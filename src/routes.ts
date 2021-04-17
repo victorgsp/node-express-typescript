6e07693c8c4a2d0131029f7604b89b25c2688407
Victor <vgsp@cesar.org.br>
2021-04-16 22:45:32 -0300

    code adjustment

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,10 @@ import { createUserController } from "./useCases/CreateUser";
 const router = Router();
 
 router.get("/users", (req: Request, res: Response) => {
-  return res.status(201).json({ users: ["user1","user2"] }).send();
+  return res
+    .status(201)
+    .json({ users: ["user1", "user2"] })
+    .send();
 });
 
 router.post("/users", (req: Request, res: Response) => {
